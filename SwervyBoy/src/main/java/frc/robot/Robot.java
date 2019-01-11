1cf95b1d4901f0e6007c5f2f034f4548778920e9
jamiha kammeraad <jamihakammeraad25@gmail.com>
2019-01-10 20:40:20 -0500

    Commit Code 4
    Number 3 was a mistake.

--- SwervyBoy/src/main/java/frc/robot/Robot.java
+++ SwervyBoy/src/main/java/frc/robot/Robot.java
@@ -1,28 +1,59 @@
 package frc.robot;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.analog.adis16448.frc.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.VictorSP;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  XboxController Controller;
   Joystick Stick;
   SwerveDriveConfig config;
   SwerveDrive Swerve;
+
+  VictorSP LFVictor;
+  VictorSP RFVictor;
+  VictorSP BLVictor;
+  VictorSP BRVictor;
+
+  TalonSRX LFTalon;
+  TalonSRX RFTalon;
+  TalonSRX BLTalon;
+  TalonSRX BRTalon;
+
+  ADIS16448_IMU gyro;
+
   public void robotInit() {
+    LFTalon = new TalonSRX(0);
+    RFTalon = new TalonSRX(1);
+    BLTalon = new TalonSRX(2);
+    BRTalon = new TalonSRX(3);
+
+    LFVictor = new VictorSP(0);
+    RFVictor = new VictorSP(1);
+    BLVictor = new VictorSP(2);
+    BRVictor = new VictorSP(3);
+
+    gyro = new ADIS16448_IMU();
+    gyro.calibrate();
     config = new SwerveDriveConfig();
-    config.wheels
-    config.summarizeTalonErrors
-    config.robotPeriod
-    config.length
-    config.gyroRateCoeff
-    config.gyroLoggingEnabled
-    config.gyro
+    config.gyro = gyro;
+    config.width = 27.5;
+    config.length = 31;
+
+    config.wheels[0] = new Wheel(LFTalon, LFVictor, 1.0);
+    config.wheels[1] = new Wheel(RFTalon, RFVictor, 1.0);
+    config.wheels[2] = new Wheel(BLTalon, BLVictor, 1.0);
+    config.wheels[3] = new Wheel(BRTalon, BRVictor, 1.0);
+
     Swerve = new SwerveDrive(config);
     Stick = new Joystick(1);
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
@@ -33,10 +64,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
 
-    double value;
-    value = Stick.getX();
-    value = Stick.getY();
-    value = Stick.getZ();
+    Swerve.drive(Stick.getY(), Stick.getX(), Stick.getZ());
   }
 
   @Override
