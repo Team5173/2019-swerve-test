91c84b7fceb5f8e248b9c85440696b97a5738158
jamiha kammeraad <jamihakammeraad25@gmail.com>
2019-01-20 14:41:58 -0500

    Swerve Drive Gryo "Fixed"
    This might fix the gyro issue after some extensive research on how to send gyro information to dashboard.

--- SwervyBoy/src/main/java/frc/robot/Robot.java
+++ SwervyBoy/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -13,30 +6,22 @@ import com.analog.adis16448.frc.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Compressor;
 
-/*
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  DoubleSolenoid wooow = new DoubleSolenoid (1,2);
-  DoubleSolenoid woow = new DoubleSolenoid (1,2);
-  DoubleSolenoid wow = new DoubleSolenoid (1,2);
-  XboxController controller = new XboxController(0);
 
   XboxController Controller;
   Joystick Stick;
+
   SwerveDriveConfig config;
   SwerveDrive Swerve;
+
+  Compressor Air;
 
   VictorSP LFVictor;
   VictorSP RFVictor;
@@ -64,10 +49,20 @@ public class Robot extends TimedRobot {
     BLVictor = new VictorSP(2);
     BRVictor = new VictorSP(3);
 
+    Swerve = new SwerveDrive(config);
+
+    Stick = new Joystick(1);
+    Controller = new XboxController(2);
+
+    Air = new Compressor();
+
     gyro = new ADIS16448_IMU();
+
     config = new SwerveDriveConfig();
 
     config.gyro = gyro;
+    gyro.reset();
+
     config.width = 27.5;
     config.length = 31;
 
@@ -75,72 +70,38 @@ public class Robot extends TimedRobot {
     RFWheel = new Wheel(RFTalon, RFVictor, 1.0);
     LBWheel = new Wheel(BLTalon, BLVictor, 1.0);
     RBWheel = new Wheel(BRTalon, BRVictor, 1.0);
+
     wheels = new Wheel[]{
+
       LFWheel, RFWheel, LBWheel, RBWheel
+
     };
+
     config.wheels = wheels;
-
-    Swerve = new SwerveDrive(config);
-    Stick = new Joystick(1);
-
-    Controller = new XboxController(2);
 
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
   public void robotPeriodic() {
 
     Swerve.drive(Stick.getY(), Stick.getX(), Stick.getZ()); 
 
-    SmartDashboard.putNumber("Gyro-X", gyro.getAngleX());
-    SmartDashboard.putNumber("Gyro-Y", gyro.getAngleY());
-    SmartDashboard.putNumber("Gyro-Z", gyro.getAngleZ());
-    
-    SmartDashboard.putNumber("Accel-X", gyro.getAccelX());
-    SmartDashboard.putNumber("Accel-Y", gyro.getAccelY());
-    SmartDashboard.putNumber("Accel-Z", gyro.getAccelZ());
-    
-    SmartDashboard.putNumber("Pitch", gyro.getPitch());
-    SmartDashboard.putNumber("Roll", gyro.getRoll());
-    SmartDashboard.putNumber("Yaw", gyro.getYaw());
-    
-    SmartDashboard.putNumber("Pressure: ", gyro.getBarometricPressure());
-    SmartDashboard.putNumber("Temperature: ", gyro.getTemperature());
+    SmartDashboard.putNumber("Gyro angle", gyro.getAngle());
+
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
+
+    gyro.reset();
+
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
       case kCustomAuto:
@@ -153,30 +114,11 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
   public void teleopPeriodic() {
-    if(controller.getAButton() == true) {
-      wow.set(DoubleSolenoid.Value.kForward);
-    }
-    if(controller.getBButton() == true) {
-      woow.set(DoubleSolenoid.Value.kForward);
-    }
-    if(controller.getXButton() == true) {
-      wooow.set(DoubleSolenoid.Value.kForward);
-    }
+    //Called during Teleop Mode
     }
 
-
-    
-
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
   public void testPeriodic() {
+    //Called during TestMode
   }
 }
